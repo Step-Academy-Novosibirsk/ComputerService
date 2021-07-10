fe7b0960e07601f4aa271e313d0a3dd697269669
Oleg Golenischev <oleg-golenischev63@yandex.ru>
2021-07-10 16:58:51 +0700

    сделан вывод списка клиентов

--- src/main/resources/static/js/clients.js
+++ src/main/resources/static/js/clients.js
@@ -1,3 +1,14 @@
-function test(){
-    alert('hello world!');
+async function loadClients(){
+  let response =  await fetch("http://localhost:8080/api/Clients/");
+  if(response.ok) {
+      let clients = await response.json();
+      let list = document.getElementById('Clients');
+      for(let i=0;i<clients.length;i++) {
+          let name = document.createElement('li');
+          name.innerText = clients[i].name;
+          list.appendChild(name);
+      }
+  }
+  else
+      alert("request has been failed");
 }
